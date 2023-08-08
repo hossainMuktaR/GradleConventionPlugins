6265cdd64dc011194a460b18c974c170ce380efa
Hossain MuktaR <42916907+hossainMuktaR@users.noreply.github.com>
2023-08-09 03:23:29 +0600

    added binary plugin ['androidApplication','DependenciesConvention']

--- build-logic-binary/convention/build.gradle.kts
+++ build-logic-binary/convention/build.gradle.kts
@@ -1,3 +1,23 @@
 plugins {
     `kotlin-dsl`
 }
+
+group = "com.example.convention"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "org.example.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("appDependencies") {
+            id = "org.example.app.dependencies"
+            implementationClass = "DependenciesConventionPlugin"
+        }
+    }
+}
---
+++ build-logic-binary/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,54 @@
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        with(project) {
+            with(pluginManager) {
+                apply("com.android.application")
+                apply("org.jetbrains.kotlin.android")
+            }
+            extensions.configure<ApplicationExtension> {
+                compileSdk = 33
+                defaultConfig {
+                    minSdk = 24
+                    targetSdk = 33
+                    versionCode = 1
+                    versionName = "1.0"
+
+                    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                    vectorDrawables {
+                        useSupportLibrary = true
+                    }
+                }
+                buildTypes {
+                    release {
+                        isMinifyEnabled = false
+                        proguardFiles(
+                            getDefaultProguardFile("proguard-android-optimize.txt"),
+                            "proguard-rules.pro"
+                        )
+                    }
+                }
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
+                }
+                buildFeatures {
+                    compose = true
+                }
+                composeOptions {
+                    kotlinCompilerExtensionVersion = "1.5.1"
+                }
+                packaging {
+                    resources {
+                        excludes += "/META-INF/{AL2.0,LGPL2.1}"
+                    }
+                }
+            }
+        }
+    }
+}
